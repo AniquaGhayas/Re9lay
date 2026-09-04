98aeec6dba566452b2eb42bd346ad93dc3740ec9
Shaik Muhammed Muzakir <121695048+killingtech15@users.noreply.github.com>
2026-09-04 14:21:19 +0530

    Fix formatting and ensure newline at end of file

--- Firmware/NeuroPlay_Arduino.ino
+++ Firmware/NeuroPlay_Arduino.ino
@@ -9,7 +9,7 @@ SoftwareSerial BTSerial(10, 11); // RX, TX
 
 void setup() {
   Serial.begin(115200);
-  BTSerial.begin(9600);   // SoftwareSerial Bluetooth baud rate: 9600
+  BTSerial.begin(9600);   
   pinMode(emgPin, INPUT);
   Wire.begin();
 
